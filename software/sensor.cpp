85567c0179572f1850fa3c67bf204d081fa7bb28
Filips Talums <jv9xdqr2m4@privaterelay.appleid.com>
2026-05-15 12:25:06 +0100

    Update sensor.cpp

--- software/sensor.cpp
+++ software/sensor.cpp
@@ -1,18 +1,34 @@
 #include "sensor.h"
-#include "config.g"
+#include "config.h"
 #include <Wire.h>
 #include <INA226.h>
 
-boot initaliseSensor() {
+INA226 ina226(INA226_ADDRESS); 
+
+bool initialiseSensor() {
   Wire.begin(SDA_PIN, SCL_PIN);
-
+  
   if (ina226.begin() == false) {
-    return flase;
-
+    return false;
   }
-
+  
   ina226.setMaxCurrentShunt(MAXIMUM_CURRENT, SHUNT_VAL);
-
   return true;
+}
 
+
+float getVoltage() {
+  float voltage = ina226.getVoltage();
+  return voltage;
+}
+
+
+float getCurrent() {
+  float current = ina226.getCurrent();
+  return current;
+}
+
+float getPower() {
+  float power = ina226.getPower();
+  return power;
 }
